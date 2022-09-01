ded3eacf422102bb8fe86e2baca9450a65ba7a30
Gabriel Auger <gauger@edu.cegepgarneau.ca>
2022-09-01 18:14:28 -0400

    Ajout Jump et TileMap + Tile Palette

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     public float dirX;
     public bool faitFaceAGauche;
     public float runSpeed;
+    public bool isGrounded;
+    private bool jumpButton;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,28 +23,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.25f, groundLayer);
+        jumpButton = Input.GetButtonDown("Jump");
+        if (jumpButton && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.y, 5f);
+        }
     }
 
     // FixedUpdate est utilisé pour gérer les physiques et simplifie l'usage de diverses fonctionalités
     void FixedUpdate()
     {
-        //Le offset est défini arbitrairement pour déterminer la distance de l'ajustement (servira plus tard).
         //Le if_else sert à ajuster la position du sprite lors d'un changement de direction.
         //Rb.velocity va changer la vitesse horizontale du joueur avec un Vecteur prenant en parametre dirX.
 
         dirX = Input.GetAxis("Horizontal");
         if ((dirX > 0 && faitFaceAGauche || dirX < 0 && !faitFaceAGauche))
         {
-            float offset = 0.25f;
-            if (dirX > 0)
-            {
-                transform.position = new Vector3(rb.position.x + offset, rb.position.y);
-            }
-            else if (dirX < 0)
-            {
-                transform.position = new Vector3(rb.position.x - offset, rb.position.y);
-
-            }
             faitFaceAGauche = !faitFaceAGauche;
             transform.Rotate(new Vector3(0, 180, 0));
         }
