ea2e7e9c2808be3b75cf3c5714404324b8c1e213
GabrielAuger1 <gauger@edu.cegepgarneau.ca>
2022-10-04 20:07:34 -0400

    Fix : Fichiers redondant effacés

--- Assets/SaveFiles/Player.cs
+++ Assets/SaveFiles/Player.cs
@@ -9,13 +9,6 @@ public class Player : MonoBehaviour
     public PlayerHealth playerHealth;
     public int health;
     public float[] position = new float[3];
-    void Update()
-    {
-        health = playerHealth.currentHealth;
-        position[0] = rb.transform.position.x;
-        position[1] = rb.transform.position.y;
-        position[2] = rb.transform.position.z;
-    }
 
     public void SavePlayer()
     {
